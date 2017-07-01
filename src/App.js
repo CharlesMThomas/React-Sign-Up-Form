bee13d03728e70f76e46a24246fb5e52d6a3666a
Charles Thomas <charles@charlesmthomas.com>
2017-07-01 09:42:34 -0400

    Add Bootstrap and create sign up form

--- src/App.js
+++ src/App.js
@@ -3,16 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      formType: 'signUp'
+    }
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+          <h2>Create a New Account</h2>
+          <p>Already a member? Login</p>
+          <form>
+            <div className="form-group">
+              <input type="email" className="form-control" placeholder="Email" />
+            </div>
+            <div className="form-group">
+              <input type="pasword" className="form-control" placeholder="New Password" />
+            </div>
+            <div className="form-group">
+              <input type="pasword" className="form-control" placeholder="Re-enter Password" />
+            </div>
+            <button type="submit" className="btn btn-success btn-block">Create Account</button> 
+          </form>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }

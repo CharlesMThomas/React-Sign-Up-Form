c9f7fdd2f8d669c8fa975bc793c89777d6cbd9b5
Charles Thomas <charles@charlesmthomas.com>
2017-07-01 11:22:05 -0400

    Add remaining sign up form validation checks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import CriteriaMessage from './containers/criteriaMessage.js';
 
@@ -9,16 +8,29 @@ class App extends Component {
 
     this.state = {
       formType: 'signUp',
-      email: '',
+      username: '',
       pw: '',
       pwConfirm: '',
-      lengthCheck: false
+      lengthCheck: false,
+      lettersNumbersCheck: false,
+      uppercaseCheck: false,
+      specialCharCheck: false,
+      pwMatchCheck: false,
+      formValid: false
     }
   }
 
   pwUpdate(pw) {
-    this.setState({ pw });
     this.lengthCheck(pw);
+    this.lettersNumbersCheck(pw);
+    this.uppercaseCheck(pw);
+    this.specialCharCheck(pw);
+    this.setState({ pw });
+  }
+
+  pwConfirmUpdate(pwConfirm) {
+    this.pwMatchCheck(pwConfirm);
+    this.setState({ pwConfirm });
   }
 
   lengthCheck(pw) {
@@ -26,6 +38,42 @@ class App extends Component {
         this.setState({ lengthCheck: true });
     } else {
         this.setState({ lengthCheck: false });
+    }
+  }
+
+  uppercaseCheck(pw) {
+    let uppercase = pw.match(/[A-Z]/);
+    if (uppercase) {
+      this.setState({ uppercaseCheck: true });
+    } else {
+      this.setState({ uppercaseCheck: false });
+    }
+  }
+
+  lettersNumbersCheck(pw) {
+    let letters = pw.match(/[a-zA-Z]/);
+    let numbers = pw.match(/[0-9]/);
+    if (letters && numbers) {
+      this.setState({ lettersNumbersCheck: true })
+    } else {
+      this.setState({ lettersNumbersCheck: false })
+    }
+  }
+
+  specialCharCheck(pw) {
+    let specialChar = pw.match(/[!@#$%^&*)(+=._-]/);
+    if (specialChar) {
+      this.setState({ specialCharCheck: true });
+    } else {
+      this.setState({ specialCharCheck: false });
+    }
+  }
+
+  pwMatchCheck(pwConfirm) {
+    if (this.state.pw === pwConfirm) {
+      this.setState({  pwMatchCheck: true });
+    } else {
+      this.setState({ pwMatchCheck: false });
     }
   }
 
@@ -43,11 +91,11 @@ class App extends Component {
             <form>
               <div className="form-group">
                 <input 
-                  value={this.state.email}
-                  onChange={event => this.setState({email: event.target.value})}
-                  type="email" 
+                  value={this.state.username}
+                  onChange={event => this.setState({username: event.target.value})}
+                  type="text" 
                   className="form-control" 
-                  placeholder="Email" />
+                  placeholder="Username or Email" />
               </div>
               <div className="form-group">
                 <input 
@@ -60,12 +108,12 @@ class App extends Component {
               <div className="form-group">
                 <input 
                   value={this.state.pwConfirm}
-                  onChange={event => this.setState({pwConfirm: event.target.value})}
+                  onChange={event => this.pwConfirmUpdate(event.target.value)}
                   type="pasword" 
                   className="form-control"
                   placeholder="Re-enter Password" />
               </div>
-              <button type="submit" className="btn btn-success btn-block">Create Account</button> 
+              <button type="submit" className='btn btn-success btn-block' >Create Account</button> 
             </form>
           </div>
           <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
@@ -74,10 +122,10 @@ class App extends Component {
                 <h3>Password Criteria</h3>
                 <ul>
                   <CriteriaMessage valid={this.state.lengthCheck} msg='Password must be more than 8 characters' />
-                  <li className="text-left"><span className="glyphicon glyphicon-remove-circle" aria-hidden="true"></span> Password must both letters and numbers.</li>
-                  <li className="text-left"><span className="glyphicon glyphicon-remove-circle" aria-hidden="true"></span> Password must contain at least 1 uppercase letter.</li>
-                  <li className="text-left"><span className="glyphicon glyphicon-remove-circle" aria-hidden="true"></span> Password must contain at least 1 special character.</li>
-                  <li className="text-left"><span className="glyphicon glyphicon-remove-circle" aria-hidden="true"></span> Password must match.</li>
+                  <CriteriaMessage valid={this.state.lettersNumbersCheck} msg='Password must both letters and numbers.' />
+                  <CriteriaMessage valid={this.state.uppercaseCheck} msg='Password must contain at least 1 uppercase letter.' />
+                  <CriteriaMessage valid={this.state.specialCharCheck} msg='Password must contain at least 1 special character.' />
+                  <CriteriaMessage valid={this.state.pwMatchCheck} msg='Password must match.' />
                 </ul>
               </div>
             </div>
